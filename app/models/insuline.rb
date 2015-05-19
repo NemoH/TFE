a35c1ff4eeabd37fa4fe4106b68111ad52573438
BERTRAND <hautier.bertrand@gmail.com>
2015-05-19 15:35:28 +0200

    ajout des choix préremplis

--- app/models/insuline.rb
+++ app/models/insuline.rb
@@ -1,7 +1,11 @@
 class Insuline < ActiveRecord::Base
 belongs_to :user
+
+TYPE = ['Novorapid', 'Levemir','Novolog','Humulin R','Lantus']
 def set_user!(user)
   	self.user_id = user.id
   	self.save!
  end
+
+
 end
